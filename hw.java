a08e03a08518ed763a9ed98189c317fc5e6298c1
Stullif <sturlaf7@gmail.com>
2015-03-11 15:36:36 +0000

    Bætti við HelloWorld

---
+++ hw.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Stulli
+ *
+ */
+public class hw {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Shclorp!");
+	}
+
+}
