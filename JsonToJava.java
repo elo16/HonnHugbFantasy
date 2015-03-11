8cb8e7bfa47f456d3ae1b1f91b916b0bde4fa8c3
Stullif <sturlaf7@gmail.com>
2015-03-11 16:39:30 +0000

    Setti inn einfaldan Gson constructor fyrir test

--- JsonToJava.java
+++ JsonToJava.java
@@ -1,9 +1,12 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class JsonToJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Gson gson = new GsonBuilder().create();
+		gson.toJson("Hello!", System.out);
 	}
 
 }
