30abf30c72d1c5e83482c3d19820a421f65285f7
Stullif <sturlaf7@gmail.com>
2015-03-15 20:03:35 +0000

    Byrjun á mockPlayer.java

---
+++ mockPlayer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Stulli
+ *
+ */
+public class mockPlayer {
+	private String playerName;
+	private int injuryStatus;
+	private String position;
+	private Stats;
+
+	public mockPlayer(){
+	this.playerName = "Olivier Giroud";
+	this.injuryStatus = 0;
+	this.position = "Striker";
+	this.stats;
+}
