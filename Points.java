7e06bfcd2f973ecf653ae1274befd8eca045f12e
Stullif <sturlaf7@gmail.com>
2015-03-25 17:29:35 +0000

    Bætti við markaskorunar function í points og það voru einhver strik sem Kristín setti inní JsonToJava sem ég seivaði bara

--- Points.java
+++ Points.java
@@ -50,14 +50,23 @@ public class Points {
 	public int getPoints(){
 		int sum = 0;
 		sum += this.appearance;
-		//sum += this.goalPoints();
+		sum += this.goalPoints(this.goalScored, this.goalAssist, this.ownGoal, this.player);
 		sum += this.goalAssist *3;
-		//sum += this.cleanSheetPoints()
-		//sum += this.penaltyPoints
-		//sum += this.yellowsAndReds()
+		//sum += this.cleanSheetPoints(this.cleanSheet, this.player)
+		//sum += this.penaltyPoints(this.penaltySaved, this.missedPenalty, this.player)
+		//sum += this.yellowsAndReds(this.yellowCards, this.redCards)
 		sum += this.inDreamTeam *3;
 		return sum;
 	}
 	
-	
+	public int goalPoints(int goalScored, int goalAssist, int ownGoal, Player player){
+		int sumPoints = 0;
+		sumPoints += goalAssist *3;
+		sumPoints += ownGoal * -2;
+		String pos = player.getPosition().toLowerCase();
+		if(pos.equals("defender") || pos.equals("goalkeeper")){sumPoints += goalScored *6;}
+		if(pos.equals("midfielder")){sumPoints += goalScored *5;}
+		if(pos.equals("forward")){sumPoints += goalScored *4;}
+		return sumPoints;
+	}
 }
