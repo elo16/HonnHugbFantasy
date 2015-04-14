46454df4922585a73f5c53f5cb9cecd8644405db
Stullif <sturlaf7@gmail.com>
2015-04-14 22:21:18 +0000

    Kláraði makeTeams og gerði makeFixture, byrjaði á main falli til að geta farið að testa bráðlega

--- League.java
+++ League.java
@@ -2,15 +2,30 @@ import java.io.IOException;
 
 
 public class League {
-	private Team[] Teams;
-	private int nmbrOfTms;
+	public Team[] Teams;
+	public Game[] Fixtures;
 	
-	public League(Team[] t){
-		this.Teams = t;
-		this.nmbrOfTms = t.length;
+	public League(){
+		this.Teams = null;
+		this.Fixtures = null;
 	}
 	
-	public static void makePlayers(){
+	public void setTeams(Team[] teams){
+		this.Teams = teams;
+	}
+	
+	public Team[] getTeams(){
+		return this.Teams;
+	}
+	
+	public void setFixtures(Game[] Fix){
+		this.Fixtures = Fix;
+	}
+	
+	public Game[] getFixture(){
+		return this.Fixtures;
+	}
+	public static Player[] makePlayers(){
 		Player[] players = new Player[695];
 		for(int i = 1; i <696; i++){
 			try {
@@ -20,10 +35,12 @@ public class League {
 				e.printStackTrace();
 			}
 		}
+		return players;
 	}
 	
-	public static Team[] makeTeams(){
+	public static Team[] makeTeams(Player[] players){
 		Team[] teams = new Team[10];
+		//Create the teams and name them
 		for(int i = 0; i < teams.length; i++){
 			teams[i] = new Team();
 			switch(i){
@@ -49,13 +66,66 @@ public class League {
 				break;
 			}	
 		}
-		
-		
+		// Sorts the players into the teams
+		for(int i = 0; i < teams.length; i++){
+			String teamName = teams[i].getTeamName();
+			for(int j = 0; j < 695; j++){
+				if(teamName.equals(players[j].getTeam_name())){
+					teams[i].addPlayer(players[j]);
+				}
+			}
+		}
 		return teams;
 	}
 	
-	public static Game[] makeFixtures(League lge){
-		Game[] games = null;
+	public static Game[] makeFixtures(Team[] teams){
+		Game[] games = new Game[90];
+		int gameCount = 0;
+		for(int i = 0; i < 9; i++){
+			for(int j = i + 1;j < 10; j++){
+				games[gameCount] = new Game();
+				games[gameCount].setHomeTeam(teams[i]);
+				games[gameCount].setAwayTeam(teams[j]);
+				
+				games[gameCount+45] = new Game();
+				games[gameCount+45].setHomeTeam(teams[j]);
+				games[gameCount+45].setAwayTeam(teams[i]);
+				
+				gameCount += 2;
+			}
+		}
+		shaker(games);
 		return games;
 	}
+	
+	public static void shaker(Game[]games){
+		int n = 2000;
+		while(n>=1){
+			int a = (int) Math.floor(Math.random()*45);
+			int b = (int) Math.floor(Math.random()*45);
+			
+			Game t = new Game();
+			t = games[a];
+			games[a] = games[b];
+			games[b] = t;
+			
+			a = (int) Math.floor(Math.random()*45 +45);
+			b = (int) Math.floor(Math.random()*45 +45);
+			
+			t = games[a];
+			games[a] = games[b];
+			games[b] = t;
+			
+		}
+	}
+
+	public static void main(String[] args){
+		League leag = new League();
+		Team[] teams = makeTeams(makePlayers());
+		leag.setTeams(teams);
+		Game[] Fix = makeFixtures(teams);
+		leag.setFixtures(Fix);
+		
+	}
 }
+
